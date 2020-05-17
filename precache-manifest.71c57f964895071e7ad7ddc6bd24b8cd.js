3b4df6e6fb033a9fefd037774e7a478473b30a0a
unknown <youmari300@gmail.com>
2020-05-17 00:09:34 +0000

    Updates

---
+++ precache-manifest.71c57f964895071e7ad7ddc6bd24b8cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d3a1bb75fb3eff2b4c878f1f496321",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "9a2ec41fd4395508cf1d",
+    "url": "/Covid-19/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "99f322523e170c80fb94",
+    "url": "/Covid-19/static/css/main.1ae11991.chunk.css"
+  },
+  {
+    "revision": "9a2ec41fd4395508cf1d",
+    "url": "/Covid-19/static/js/2.0202eaf8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19/static/js/2.0202eaf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99f322523e170c80fb94",
+    "url": "/Covid-19/static/js/main.6c17f49b.chunk.js"
+  },
+  {
+    "revision": "d369a12630010b981edd",
+    "url": "/Covid-19/static/js/runtime-main.42555f4d.js"
+  },
+  {
+    "revision": "80f549028ff89240788c732cec3a54a2",
+    "url": "/Covid-19/static/media/19.80f54902.png"
+  }
+]);
